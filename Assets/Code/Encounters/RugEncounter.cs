0258e7d98a93912e0940b6092052ea358ff9e5a9
Dawid Kaluza <dawid.kaluza@tensquaregames.com>
2022-05-14 18:07:20 +0200

    rug and mirror graphics

--- Assets/Code/Encounters/RugEncounter.cs
+++ Assets/Code/Encounters/RugEncounter.cs
@@ -1,42 +1,47 @@
-﻿using Cysharp.Threading.Tasks;
-using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Code.Encounters
 {
     public class RugEncounter : BaseEncounter
     {
-        [SerializeField] private Transform rug;
+        /*[SerializeField] private Transform rug;
         [SerializeField] private Transform hand;
         [SerializeField] private Vector3 rugStartPosition;
         [SerializeField] private Vector3 rugEndPosition;
         [SerializeField] private float rugMoveDuration;
         [SerializeField] private Vector3 handStartPosition;
         [SerializeField] private Vector3 handEndPosition;
-        [SerializeField] private float handMoveDuration;
+        [SerializeField] private float handMoveDuration;*/
+        
+        [SerializeField] private GameObject rug;
+        [SerializeField] private GameObject rugWithHand;
+        [SerializeField] private GameObject handOnBlanket;
 
         protected override void Enable()
         {
             base.Enable();
-            Animate().Forget();
+            rugWithHand.SetActive(true);
         }
-        private async UniTask Animate()
+        /*private async UniTask Animate()
         {
             rug.DOLocalMove(rugEndPosition, rugMoveDuration);
             await UniTask.Delay((int)(rugMoveDuration * 1000));
             hand.DOLocalMove(handEndPosition, handMoveDuration);
-        }
+        }*/
         
         protected override void Activate()
         {
             base.Activate();
+            rugWithHand.SetActive(false);
+            handOnBlanket.SetActive(true);
         }
         
         protected override void Disable()
         {
             base.Disable();
-            rug.transform.position = rugStartPosition;
-            hand.transform.position = handStartPosition;
+            rugWithHand.SetActive(false);
+            handOnBlanket.SetActive(false);
+            rug.SetActive(true);
         }
     }
 }
