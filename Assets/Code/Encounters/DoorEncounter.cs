5e1c49fd326053e96ce9ba6e4d871b8bc95f767d
Dawid Kaluza <dawid.kaluza@tensquaregames.com>
2022-05-15 09:19:53 +0200

    door fix

--- Assets/Code/Encounters/DoorEncounter.cs
+++ Assets/Code/Encounters/DoorEncounter.cs
@@ -1,5 +1,4 @@
-﻿using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Code.Encounters
 {
@@ -13,21 +12,17 @@ namespace Code.Encounters
         {
             base.Enable();
             ajarDoor.gameObject.SetActive(true);
-            ajarDoor.DOFade(0f, 0f);
-            ajarDoor.DOFade(1f, 0.5f);
+            closedDoor.SetActive(false);
         }
         protected override void Activate()
         {
             base.Activate();
             openDoor.gameObject.SetActive(true);
-            openDoor.DOFade(0f, 0f);
-            openDoor.DOFade(1f, 0.5f);
+            ajarDoor.gameObject.SetActive(false);
         }
         protected override void Disable()
         {
             base.Disable();
-            ajarDoor.DOFade(0f, 0f);
-            openDoor.DOFade(0f, 0f);
             closedDoor.SetActive(true);
             openDoor.gameObject.SetActive(false);
             ajarDoor.gameObject.SetActive(false);
